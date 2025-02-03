15ae5129aaacf33461b9574f559190ba8ec5cbb9
veve196 <veve196x@gmail.com>
2025-02-03 14:01:01 +0100

    bracket

--- api.veve.dev/Controllers/DiscordController.cs
+++ api.veve.dev/Controllers/DiscordController.cs
@@ -2,35 +2,35 @@
 using Microsoft.AspNetCore.Mvc;
 using veve.Services;
 
-namespace veve.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    [Authorize]
-    public class DiscordController(ILogger<DiscordController> logger, DiscordService service) : ControllerBase
-    {
-        [HttpGet("{userId}")]
-        public async Task<IActionResult> GetUser(ulong userId)
-        {
-            try
-            {
-                var user = await service.GetUserAsnyc(userId);
-                return new JsonResult(user);
-            }
-            catch (Exception ex)
-            {
-                string errorMsg = $"Failed to get Discord user with id {userId}";
-                logger.LogError(ex, errorMsg);
-                return StatusCode(500, new
-                {
-                    error = errorMsg
-                });
-            }
-        }
+namespace veve.Controllers;
 
-        [HttpGet("spotify/{userId}")]
-        public async Task<IActionResult> GetSpotifyActivity(ulong userId)
+[ApiController]
+[Route("[controller]")]
+[Authorize]
+public class DiscordController(ILogger<DiscordController> logger, DiscordService service) : ControllerBase
+{
+    [HttpGet("{userId}")]
+    public async Task<IActionResult> GetUser(ulong userId)
+    {
+        try
         {
+            var user = await service.GetUserAsnyc(userId);
+            return new JsonResult(user);
+        }
+        catch (Exception ex)
+        {
+            string errorMsg = $"Failed to get Discord user with id {userId}";
+            logger.LogError(ex, errorMsg);
+            return StatusCode(500, new
+            {
+                error = errorMsg
+            });
+        }
+    }
+
+    [HttpGet("spotify/{userId}")]
+    public async Task<IActionResult> GetSpotifyActivity(ulong userId)
+    {
         {
             try
             {
