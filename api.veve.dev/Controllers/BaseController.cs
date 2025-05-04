b1eb8846dd86dc15d5bbc2594b97b6b5f6231495
veve196 <veve196x@gmail.com>
2025-05-04 14:54:30 +0200

    datetime

--- api.veve.dev/Controllers/BaseController.cs
+++ api.veve.dev/Controllers/BaseController.cs
@@ -6,6 +6,12 @@ namespace veve.Controllers
     {
         protected IWebHostEnvironment WebHostEnvironment => hostEnvironment;
 
+        [HttpGet("DateTime")]
+        public IActionResult GetCurrentDateTime() => Ok(DateTime.Now);
+
+        [HttpGet("UtcDateTime")]
+        public IActionResult GetCurrentUtcDateTime() => Ok(DateTime.UtcNow);
+
         protected string GetErrorDetails(Exception ex)
         {
             return WebHostEnvironment.IsDevelopment() ? ex.Message : "Switch to development mode to see details.";
